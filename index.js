688462d91e3ac7e8c7ee0fb27b44c51beb66df56
Brad Morgan <bmorgan1@urbn.com>
2022-08-23 14:22:16 -0400

    Added header flags and fixed ordering

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-const { Kafka } = require('kafkajs')
+const { Kafka } = require("kafkajs")
 const fetch = require("node-fetch")
-const isEqual = require('lodash.isequal');
+const isEqual = require("lodash.isequal");
 
 const kafka = new Kafka({
-  clientId: 'outbox-tester',
-  brokers: ['localhost:9092'],
+  clientId: "outbox-tester",
+  brokers: ["localhost:9092"],
 })
 
 const admin = kafka.admin()
 
-const consumer = kafka.consumer({ groupId: 'outbox-tester-group' })
+const consumer = kafka.consumer({ groupId: "outbox-tester-group" })
 
 const returnedEventIds = []
 const consumedMessageKeys = []
+
+async function main() {
+  await consumeAllMessages()
+
+  let count = 0
+  const interval = setInterval(async () => {
+    if (count++ > 10) {
+      clearInterval(interval);
+      try {
+        await retryWithCount(20, 250, () => {
+          if (!isEqual(new Set(returnedEventIds), new Set(consumedMessageKeys))) {
+            throw new Error("Mismatch between messages sent and consumed")
+          } else if (!isEqual(returnedEventIds, consumedMessageKeys)) {
+            throw new Error("Mismatch message order/duplicates")
+          }
+        })
+      } catch (e) {
+        console.error(`Failed with error: ${e.message}`)
+        console.log({
+          returnedEventIds,
+          consumedMessageKeys
+        })
+      } finally {
+        await consumer.disconnect()
+        process.exit(0)
+      }
+    }
+    postMessage()
+  }, 100);
+}
+ main()
 
 async function consumeAllMessages() {
   await admin.deleteTopics({
@@ -27,7 +58,7 @@ async function consumeAllMessages() {
   })
 
   await consumer.connect()
-  await consumer.subscribe({ topic: 'outbox_topic' })
+  await consumer.subscribe({ topic: "outbox_topic" })
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
@@ -36,44 +67,21 @@ async function consumeAllMessages() {
   })
 }
 
-consumeAllMessages()
-
 async function postMessage() {
   const response = await fetch("http://localhost:8080/outbox", {
     method: "POST",
     body: '{"foo": "bar"}',
-    headers: {'Content-Type': 'application/json'}
+    headers: {
+      "Content-Type": "application/json",
+      "X-TEST-SEND-DELAY-MS": 0,
+      "X-TEST-DELETE-DELAY-MS": 0,
+      "X-TEST-DELETE-PERCENT": 50,
+    }
   })
   if (response.ok) {
     returnedEventIds.push(await response.text())
   }
 }
-
-let count = 0
-const interval = setInterval(async () => {
-  if (count++ > 10) {
-    clearInterval(interval);
-    try {
-      await retryWithCount(20, 250, () => {
-        if (!isEqual(new Set(returnedEventIds), new Set(consumedMessageKeys))) {
-          throw new Error("Mismatch between messages sent and consumed")
-        } else if (!isEqual(returnedEventIds, consumedMessageKeys)) {
-          throw new Error("Mismatch message order/duplicates")
-        }
-      })
-    } catch (e) {
-      console.error(`Failed with error: ${e.message}`)
-      console.log({
-        returnedEventIds,
-        consumedMessageKeys
-      })
-    } finally {
-      await consumer.disconnect()
-      process.exit(0)
-    }
-  }
-  postMessage()
-}, 100);
 
 async function retryWithCount(
     count,
